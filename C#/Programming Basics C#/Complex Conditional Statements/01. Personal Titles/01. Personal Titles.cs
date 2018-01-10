7ccc8bb265ad0347b79d26b5b1d3e96a789991d5
aldpeev <aldpeev@gmail.com>
2018-01-10 13:23:12 +0200

    Added Programming Basics C# Complex Conditional Statements

---
+++ C#/Programming Basics C#/Complex Conditional Statements/01. Personal Titles/01. Personal Titles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Personal_Titles
+{
+    class _01
+    {
+        static void Main(string[] args)
+        {
+            double agre = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (gender == "m" && agre >= 16)
+            {
+                Console.WriteLine("Mr.");
+            }
+            if (gender == "m" && agre < 16)
+            {
+                Console.WriteLine("Master");
+            }
+            if (gender == "f" && agre >= 16)
+            {
+                Console.WriteLine("Ms.");
+            }
+            if (gender == "f" && agre < 16)
+            {
+                Console.WriteLine("Miss");
+            }
+        }
+    }
+}
