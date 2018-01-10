91c94efd18ac1885b130e422a013a32e124f0eb8
aldpeev <aldpeev@gmail.com>
2018-01-10 13:35:36 +0200

    Added First Steps in Coding

---
+++ C#/Programming Basics C#/First Steps in Coding/04. Triangle of 55 Stars/04. Triangle of 55 Stars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    class stars
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 1; row <= 10; row++)
+            {
+                for (int col = 0; col < row; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ C#/Programming Basics C#/First Steps in Coding/06. Square of Stars/06. Square of Stars.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('*', n));
+
+            for (int row = 0; row < n - 2; row++)
+            {
+                Console.Write("*");
+                for (int col = 0; col < n - 2; col++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("*");
+
+            }
+
+            Console.WriteLine(new string('*', n));
+
+
+        }
+    }
+}
